b6280bf0d0022ed02bcf4c9d73568c15b041963a
Tomotaka Sakuma <ktmtmks@gmail.com>
2014-08-31 13:05:59 +0900

    echo handler

---
+++ templates/etc/hoko.d/echo.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+echo
+echo "------- $0"
+echo "SERF_EVENT: ${SERF_EVENT}"
+echo "SERF_QUERY_NAME: ${SERF_QUERY_NAME}"
+echo "SERF_SELF_NAME: ${SERF_SELF_NAME}"
+echo "SERF_TAG_WEBHOOK: ${SERF_TAG_WEBHOOK}"
+echo "HOKO_VERSION: ${HOKO_VERSION}"
+echo "HOKO_PATH: ${HOKO_PATH}"
+echo "payload: $(cat)"
+echo
+echo
+
